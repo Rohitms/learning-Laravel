4e0c2c310a5347e55b3c08a799768fd55a5c0030
Rohitms <rohithmsasi@gmail.com>
2017-02-17 09:35:37 +0530

    bla

---
+++ app/Http/Controllers/helloController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class helloController extends Controller
+{
+    //
+    public function ggwp(){
+    return view ("calc");}
+    public function add(Request $request){
+    $sum=$request->number1+$request->number2;      
+    return view('sum',['result'=>$sum]);
+}
+}
---
+++ resources/views/sum.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title> blabla
+</title>
+R E D
+</head>
+<body>
+<label>Sum is:{{$result}} </label>
+</body>
+</html>
